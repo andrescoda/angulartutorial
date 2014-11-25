ed3251dae70bda3a134573e5c9dd49b6a1ef2a0a
andres <andres.coda@globant.com>
2014-11-25 09:03:01 -0300

    fix topic3-4

--- topic3/test.js
+++ topic3/test.js
@@ -1,10 +1,13 @@
 'use strict';
 
-angular.module('myApp')
-	.controller('MainCtrl', function MainCtrl($scope){
-		$scope.name = "Andres";
-		$scope.lastname = "Coda"
-		$scope.gender = "M";
-		$scope.age = "30 years";
-		$scope.superpower = "Bionic audition";
+angular.module('myApp', [])
+			.controller('MainController',
+				function MainController($scope){
+					$scope.name = "Andres";
+					$scope.lastname = "Coda";
+					$scope.gender = "M";
+					$scope.age = "30 years";
+					$scope.superpower = "Bionic audition";
 	});
+
+	
